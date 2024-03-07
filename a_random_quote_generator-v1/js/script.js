090fe2337bbdc01aaad8b83d5e73850da24f30a0
LealxJS <iangameplay101@gmail.com>
2024-03-07 18:57:16 +0800

    Update script.js
    edited for the revision of quotes to quote

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,62 +11,62 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 const quotes = [{
-  quotes:'Happiness is not by chance but by choice.',
+  quote:'Happiness is not by chance but by choice.',
   source:'Jim Rohn',
   citation:'Google',
   year:'',
 
 }, {
-  quotes:'All the world\'s a stage, and all the men and women merely players.',
+  quote:'All the world\'s a stage, and all the men and women merely players.',
   source:'William Shakespeare',
   citation:'from As You Like It',
   year:'1599',
   tags:'#Lifequotes',
 }, {
-  quotes:'To love is to burn, to be on fire',
+  quote:'To love is to burn, to be on fire',
   source:'Emma Thompson',
   citation:'Sense and Sensibility screenplay',
   year:'1995',
 }, {
-  quotes:'Take my hand, take my whole life too. For I can\'t help falling in love with you.',
+  quote:'Take my hand, take my whole life too. For I can\'t help falling in love with you.',
   source:'Elvis Presley',
   citation:'Can\'t Help Falling In Love song',
   year:'1961',
   tags:'#Song, #LoveQuote',
 }, {
-  quotes:'The only way to do great work is to love what you do.',
+  quote:'The only way to do great work is to love what you do.',
   source:'Steve Jobs, Co-Founder of Apple Inc.',
   citation:'Commencement Speech at Standford University',
   year:'2005',
   tags:'#Motivation, #Success, #Work',
 }, {
-  quotes:'In the end, we only regret the chances we didn\'t take.',
+  quote:'In the end, we only regret the chances we didn\'t take.',
   source:'Lewis Carroll, Author of "Alice\'s Adventures in WonderLand" ',
   citation:'Alice\'s Adventures in WonderLand',
   year:'1865',
   tags:'#Regret, #Risk, #LifeLesson',
 }, {
-  quotes:'Love is composed of a single soul inhabiting two bodies.',
+  quote:'Love is composed of a single soul inhabiting two bodies.',
   source:'Aristotle, Greek philosopher',
   citation:'Metaphysics',
   year:'Approximately 4th century BC',
 }, {
-  quotes:'Happiness is not something ready-made. It comes from your own actions.',
+  quote:'Happiness is not something ready-made. It comes from your own actions.',
   source:'Dalai Lama XIV, Spiritual leader of Tibetan Buddhism',
   citation:'',
   year:'',
 }, {
-  quotes:'The future belongs to those who believe in the beauty of their dreams.',
+  quote:'The future belongs to those who believe in the beauty of their dreams.',
   source:'Eleanor Roosevelt, Former First Lady of the United States',
   citation:'Speech to the graduates of the Tuskegee Institute',
   year:'1939',
 }, {
-  quotes:'The only limit to our realization of tomorrow will be our doubts of today.',
+  quote:'The only limit to our realization of tomorrow will be our doubts of today.',
   source:'Franklin D. Roosevelt, 32nd President of the United States',
   citation:'',
   year:'',
 }, {
-  quotes:'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+  quote:'The greatest glory in living lies not in never falling, but in rising every time we fall.',
   source:'Nelson Mandela, Former President of South Africa and anti-apartheid revolutionary',
   citation:'',
   year:'',
@@ -80,22 +80,6 @@ let year = document.querySelector('.year');
 let tags = document.querySelector('.tags');
 let randomRGB = [];
 
-/***
- * `getRandomQuote` function
-***/
-function getRandomQuote(){
-
-   let ranNum = Math.floor(Math.random()*quotes.length);
-   
-   if (ranNum === ranIndex){
-    return getRandomQuote();
-   } else{
-    ranIndex = ranNum;
-    return ranIndex;
-   }
-}
-
-
 /*
 
 rgbProducer is a function that will produce the numbers for the array to be used in the 
@@ -103,29 +87,44 @@ rgb colors and it automatically replaces a new set when the array is full and us
 
 */
 function rgbProducer(){
-    let number;
-    for(let i = 0; i < 3; i++){
-        number = Math.floor(Math.random() * 256);
-        randomRGB.push(number);
-    }
-    randomNGcolors();
+  let number;
+  for(let i = 0; i < 3; i++){
+      number = Math.floor(Math.random() * 256);
+      randomRGB.push(number);
+  }
+  randomNGcolors();
 
-    return randomRGB;
+  return randomRGB;
+  
 }
 const randomNGcolors = function() {
-    if (randomRGB.length > 3){
-        randomRGB.splice(0,3);
-    }
+  if (randomRGB.length > 3){
+      randomRGB.splice(0,3);
+  }
 }
 
+/**
+ *  This function gets a random number from the length of quotes
+ * @returns ranIndex to be used for getting a random quote in the getRandomQuote function
+ */
+
+function getRandomNum(){
+  let ranNum = Math.floor(Math.random()*quotes.length);
+   
+   if (ranNum === ranIndex){
+    return getRandomNum();
+   } else{
+    ranIndex = ranNum;
+    return ranIndex
+   }
+}
 /***
- * `printQuote` function
+ * `getRandomQuote` function
 ***/
-function printQuote(){
-  getRandomQuote();
-  rgbProducer();
+function getRandomQuote(){
+    getRandomNum();
     document.body.style.backgroundColor = `rgb(${randomRGB})`;
-    quote.textContent = `${quotes[ranIndex].quotes}`;
+    quote.textContent = `${quotes[ranIndex].quote}`;
     author.innerHTML = `${quotes[ranIndex].source}`;
 
     if(quotes[ranIndex].citation && !quotes[ranIndex].citation == '' && Object.keys(quotes[ranIndex]).includes('citation')){
@@ -143,8 +142,18 @@ function printQuote(){
     } else{
       tags.innerHTML = `${quotes[ranIndex].tags}`;
     }
-    
-    
+
+    console.log(`${quotes[ranIndex].quote}`);
+}
+
+
+/***
+ * `printQuote` function
+***/
+function printQuote(){
+  rgbProducer();
+  getRandomQuote();
+ 
 
   }
 /**
