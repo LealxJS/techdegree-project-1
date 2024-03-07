93edd2029cce5e5061fc8c8e73d9d7bb27447fb0
LealxJS <iangameplay101@gmail.com>
2024-03-08 03:12:37 +0800

    Update script.js
    final final final

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,75 +10,96 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-const quotes = [{
+const quotes = [
+  {
     quote: 'Happiness is not by chance but by choice.',
     source: 'Jim Rohn',
     citation: 'Google',
     year: '',
 
-  }, {
+  }, 
+  {
     quote: 'All the world\'s a stage, and all the men and women merely players.',
     source: 'William Shakespeare',
     citation: 'from As You Like It',
     year: '1599',
     tags: '#Lifequotes',
-  }, {
+  }, 
+  {
     quote: 'To love is to burn, to be on fire',
     source: 'Emma Thompson',
     citation: 'Sense and Sensibility screenplay',
     year: '1995',
-  }, {
+  }, 
+  {
     quote: 'Take my hand, take my whole life too. For I can\'t help falling in love with you.',
     source: 'Elvis Presley',
     citation: 'Can\'t Help Falling In Love song',
     year: '1961',
     tags: '#Song, #LoveQuote',
-  }, {
+  }, 
+  {
     quote: 'The only way to do great work is to love what you do.',
     source: 'Steve Jobs, Co-Founder of Apple Inc.',
     citation: 'Commencement Speech at Standford University',
     year: '2005',
     tags: '#Motivation, #Success, #Work',
-  }, {
+  }, 
+  {
     quote: 'In the end, we only regret the chances we didn\'t take.',
     source: 'Lewis Carroll, Author of "Alice\'s Adventures in WonderLand" ',
     citation: 'Alice\'s Adventures in WonderLand',
     year: '1865',
     tags: '#Regret, #Risk, #LifeLesson',
-  }, {
+  }, 
+  {
     quote: 'Love is composed of a single soul inhabiting two bodies.',
     source: 'Aristotle, Greek philosopher',
     citation: 'Metaphysics',
     year: 'Approximately 4th century BC',
-  }, {
+  }, 
+  {
     quote: 'Happiness is not something ready-made. It comes from your own actions.',
     source: 'Dalai Lama XIV, Spiritual leader of Tibetan Buddhism',
     citation: '',
     year: '',
-  }, {
+  }, 
+  {
     quote: 'The future belongs to those who believe in the beauty of their dreams.',
     source: 'Eleanor Roosevelt, Former First Lady of the United States',
     citation: 'Speech to the graduates of the Tuskegee Institute',
     year: '1939',
-  }, {
+  }, 
+  {
     quote: 'The only limit to our realization of tomorrow will be our doubts of today.',
     source: 'Franklin D. Roosevelt, 32nd President of the United States',
     citation: '',
     year: '',
-  }, {
+  }, 
+  {
     quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
     source: 'Nelson Mandela, Former President of South Africa and anti-apartheid revolutionary',
     citation: '',
     year: '',
-  }];
-  
+  }
+];
+
 const time = 2500;
-let currentIndex;
+
+
+/**
+ *  This function returns a number between 0 and the maxNumber
+ * @return number
+ */
+
+function getRandomNum(maxNumber) {
+
+  return Math.floor(Math.random() * maxNumber);
+}
 
 /*
 
-rgbProducer is a function that will produce the numbers for the array to be used in the 
-rgb colors and it automatically replaces a new set when the array is full and used.
+Function that will produce the numbers for the array to be used in randomRGB
 
 */
 function rgbProducer() {
@@ -92,21 +113,11 @@ function rgbProducer() {
 
 }
 
-/**
- *  This function returns a number between 0 and the maxNumber
- * @return number
- */
-
-function getRandomNum(maxNumber) {
-
-  return Math.floor(Math.random() * maxNumber);
-}
-
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote() {
-
+  let currentIndex;
   const randomIndex = getRandomNum(quotes.length);
 
   if (randomIndex == currentIndex) {
@@ -116,7 +127,6 @@ function getRandomQuote() {
   currentIndex = randomIndex;
   return quotes[currentIndex];
 }
-
 
 /***
  * `printQuote` function
@@ -150,10 +160,13 @@ function printQuote() {
     tags.innerHTML = newQuote.tags;
   }
 }
+
 /**
  * sets the interval for automatic use of the print quote function
  */
+
 setInterval(printQuote, time);
+
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
